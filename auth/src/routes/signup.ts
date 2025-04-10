e17b5279fd392a3c005da5a4ed20fe489df8978f
sankar-2002 <shankar.muthusamy4@gmail.com>
2025-04-10 18:25:07 +0530

    handled duplicate user (BadReqError)

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
+import { BadRequestError } from './../errors/bad-request-error';
 import { NextFunction } from 'express';
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
 import {RequestValidationError} from '../errors/request-validation-error';
-import {DatabaseConnectionError} from '../errors/database-connection-error';
+// import {DatabaseConnectionError} from '../errors/database-connection-error';
+import { User } from '../models/user';
 const router = express.Router();
 
 router.post('/api/users/signup',
@@ -22,10 +24,17 @@ router.post('/api/users/signup',
             throw new RequestValidationError(errors.array());
         }
 
-        console.log('Creating a user...');
-        throw new DatabaseConnectionError();
+        const {email, password} = req.body;
+        const existingUser = await User.findOne({email});
 
-        res.send({});
+        if(existingUser) {
+            throw new BadRequestError('Email in use');
+        }
+
+        const user = User.build({email, password});
+        await user.save();
+
+        res.status(201).send(user);
         
 });
 
