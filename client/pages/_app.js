d79f14aaa8ca4d5a3db65268afe1057a24c8f82a
sankar-2002 <shankar.muthusamy4@gmail.com>
2025-04-18 16:43:15 +0530

    depl new client srv and image, added global css

---
+++ client/pages/_app.js
@@ -0,0 +1,6 @@
+// _app.js for adding global styles and layout
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
