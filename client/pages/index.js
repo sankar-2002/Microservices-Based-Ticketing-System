483f2dcc9180c2be5990aa271fc88fb499cdad94
sankar-2002 <shankar.muthusamy4@gmail.com>
2025-04-18 22:12:30 +0530

    added signIn & signOut functionality and configured InitProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,21 @@
-export default () => {
-    return (
-        <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the main entry point of the application!!!.</p>
-        </div>
-    );
-}
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => { //browser code
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
+
+LandingPage.getInitialProps = async context => { //server code
+ //   console.log('LANDING PAGE!');
+// we'll need to pass the context to the buildClient function to do the request on Server side
+
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
