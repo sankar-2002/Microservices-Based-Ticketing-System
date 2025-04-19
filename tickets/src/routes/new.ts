fa0247728097aa406e9c609aa47b9f8d70921bd5
sankar-2002 <shankar.muthusamy4@gmail.com>
2025-04-19 16:26:39 +0530

    done with post req for ticket

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,13 +1,32 @@
 import express, { Request, Response } from 'express';
-import {requireAuth, validateRequest} from '@grstickets/common';
-
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@grstickets/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
 
-//requireAuth is a middleware that checks if the user is authenticated
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id
+    });
+    await ticket.save();
+
+    res.sendStatus(201).send(ticket);
+  }
+);
 
 export { router as createTicketRouter };
