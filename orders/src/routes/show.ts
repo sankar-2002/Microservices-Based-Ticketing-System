64ad4f9541746bf7fbe689e1488fc385276afec6
sankar-2002 <shankar.muthusamy4@gmail.com>
2025-04-22 22:02:28 +0530

    frontend done (some fixes needed)

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -13,13 +13,15 @@ router.get(
   requireAuth,
   async (req: Request, res: Response) => {
     const order = await Order.findById(req.params.orderId).populate('ticket');
-
+    console.log('order', order);
+    
     if (!order) {
       throw new NotFoundError();
     }
     if (order.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
+
 
     res.send(order);
   }
